fc8729b15ab8fc178e7749a11f3ff154fdf113c4
Samuel Le <70328135+Nhahan1997@users.noreply.github.com>
2022-04-28 21:58:44 -0500

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,6 @@
+// Middleware to check the password and check if it is authenticated, The
+// code is gotten from open source DWRSC Youtube channel. The reference can be found in README.md
+
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return res.redirect("/");
